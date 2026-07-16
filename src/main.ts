cc4e1ef6760f6bc171cba888741cd56c94b85163
daiqingyang <daiyq@suneast.com>
2026-07-16 13:17:38 +0800

    fix: move hiprint to public/, load via script tags for Vite compatibility

--- src/main.ts
+++ src/main.ts
@@ -6,15 +6,6 @@ import App from './App.vue'
 
 import './styles/global.css'
 import './styles/antd-override.css'
-import './hiprint/hiprint.css'
-import './hiprint/print-lock.css'
-
-import $ from 'jquery'
-;(window as any).$ = $
-;(window as any).jQuery = $
-
-import './hiprint/hiprint.bundle.js'
-import './hiprint/hiprint.config.js'
 
 const app = createApp(App)
 app.use(createPinia())
